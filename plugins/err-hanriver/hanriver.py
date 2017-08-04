5781ce0c5525b5f7d2ee3b8274f2d0eb08736d86
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 16:31:54 +0900

    [수정] 아닐경우도 보냄

--- plugins/err-hanriver/hanriver.py
+++ plugins/err-hanriver/hanriver.py
@@ -9,3 +9,5 @@ class HanRiver(BotPlugin):
         if mess.body.find('한강물') != -1:
             if mess.to != self.bot_identifier:
                 self.send(mess.to, "아직 준비중이에양...")
+            else:
+                self.send(mess.frm, "아직 준비중이에양...")
