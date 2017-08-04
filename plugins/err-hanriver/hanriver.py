920835f6ea3ba2650b9688ceafd10e15ae5ba066
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 16:30:17 +0900

    [수정] 셀프검사

--- plugins/err-hanriver/hanriver.py
+++ plugins/err-hanriver/hanriver.py
@@ -7,5 +7,5 @@ from errbot import BotPlugin, botcmd
 class HanRiver(BotPlugin):
     def callback_message(self, mess):
         if mess.body.find('한강물') != -1:
-            if mess.to != self._bot:
+            if mess.to != self.bot_identifier:
                 self.send(mess.to, "아직 준비중이에양...")
