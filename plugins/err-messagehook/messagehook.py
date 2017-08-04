021fec71dbe106c75a8bf66efefdb9062245abc3
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 16:46:40 +0900

    [수정] super test

--- plugins/err-messagehook/messagehook.py
+++ plugins/err-messagehook/messagehook.py
@@ -10,10 +10,11 @@ class MessageHook(BotPlugin):
     _data_list = list()
 
     def activate(self):
-    #     with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
-    #         self._data_list = f.read().splitlines()
-    #         f.close()
-        self.log.info(self._data_list)
+        # with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
+        #     self._data_list = f.read().splitlines()
+        #     f.close()
+        # self.log.info(self._data_list)
+        super().activate()
 
     def callback_message(self, mess):  # a command callable with !tryme
         """
