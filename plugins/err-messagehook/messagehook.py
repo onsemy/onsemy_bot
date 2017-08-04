1ba8adbdccc912156c60ea9a2edd629394563658
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 16:49:19 +0900

    [수정] 원복

--- plugins/err-messagehook/messagehook.py
+++ plugins/err-messagehook/messagehook.py
@@ -10,9 +10,9 @@ class MessageHook(BotPlugin):
     _data_list = list()
 
     def activate(self):
-        # with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
-        #     self._data_list = f.read().splitlines()
-        #     f.close()
+        with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
+            self._data_list = f.read().splitlines()
+            f.close()
         self.log.info(self._data_list)
         super().activate()
 
