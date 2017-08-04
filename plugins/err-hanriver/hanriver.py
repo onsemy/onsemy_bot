4991b1b9ed15829d0701debfd1688ffd46c8f43d
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 17:17:04 +0900

    [수정] 임시 파싱

--- plugins/err-hanriver/hanriver.py
+++ plugins/err-hanriver/hanriver.py
@@ -1,13 +1,31 @@
 # coding: utf-8
 
-import bot_define
+import requests
 
 from errbot import BotPlugin, botcmd
 
 class HanRiver(BotPlugin):
+    HANRIVER_URL = 'http://water.nier.go.kr/main/mainContent.do'
+    def temp_request(self):
+        result = requests.get(url = self.HANRIVER_URL)
+        content = result.text
+        find_idx = content.find('map_tit">노량진')
+        content = content[find_idx:]
+        find_idx = content.find('<th>수온(℃)</th>')
+        content = content[find_idx:]
+        find_idx = content.find('<td>')
+        content = content[find_idx:]
+        find_idx = content.find('</td>')
+        content = content[:find_idx]
+
+        return content
+
     def callback_message(self, mess):
         if mess.body.find('한강물') != -1:
-            if mess.to != self.bot_identifier:
-                self.send(mess.to, "아직 준비중이에양...")
-            else:
-                self.send(mess.frm, "아직 준비중이에양...")
+            result = self.temp_request()
+
+            target = mess.to
+            if mess.to == self.bot_identifier:
+                target = mess.frm
+
+            self.send(mess.to, "지금 한강물 " + result + "도야")                
