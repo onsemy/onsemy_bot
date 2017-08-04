0e9d6205ebff1e8f2dca43f231c8c3d5fa144760
Jeongjin Oh <jj.oh@funcrews.com>
2017-08-04 16:17:13 +0900

    [수정] 테스트

--- plugins/err-hanriver/hanriver.py
+++ plugins/err-hanriver/hanriver.py
@@ -4,4 +4,4 @@ from errbot import BotPlugin, botcmd
 class HanRiver(BotPlugin):
     def callback_message(self, mess):
         if mess.body.find('한강물') != -1:
-            self.send(mess, "아직 준비중이에양...")
+            return "아직 준비중이에양..."
