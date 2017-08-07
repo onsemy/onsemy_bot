39025fbc9cfc0ea175c21e7379ddab31f1ead691
oNsemy <onsemy@gmail.com>
2017-08-07 12:43:17 +0900

    [추가] 최적화중...

--- plugins/err-messagehook/messagehook.py
+++ plugins/err-messagehook/messagehook.py
@@ -2,14 +2,13 @@
 import os
 import datetime
 import random
+import json
 
 from errbot import BotPlugin, botcmd
 
 class MessageHook(BotPlugin):
     """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
+    메시지에서 적절한 짤방을 생산한다
     """
     _data_list = list()
     _send_group_list = list()
@@ -24,30 +23,57 @@ class MessageHook(BotPlugin):
         #             self.send(self.build_identifier(data), str(now.hour) + '시에양-!')
 
         if now.minute == 4:
-            if now.hour == 4 or now.hour == 16:
-                for data in self._send_group_list:
-                    if random.random() < 0.5:
-                        self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/nesi.jpg', 'rb'), name = 'nesi.jpg', stream_type = 'photo')
-                    else:
-                        self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/naesi.jpeg', 'rb'), name = 'naesi.jpg', stream_type = 'photo')
-            if now.hour == 2 or now.hour == 14:
-                for data in self._send_group_list:
-                    self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/doosi.jpeg', 'rb'), name = 'doosi.jpg', stream_type = 'photo')
+            for data in self._data["nesi_timer"]:
+                if data["hour"] == now.hour % 12:
+                    file_count = len(data["file_name"])
+                    file_name = data["file_name"][random.randrange(0, file_count)]
+                    for group in self._data["group_list"]:
+                        self.send_stream_request(self.build_identifier(group), open(os.getcwd() + '/resources/' + file_name, 'rb'), name = file_name, stream_type = 'photo')
+                    
+            # if now.hour == 4 or now.hour == 16:
+            #     for data in self._send_group_list:
+            #         if random.random() < 0.5:
+            #             self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/nesi.jpg', 'rb'), name = 'nesi.jpg', stream_type = 'photo')
+            #         else:
+            #             self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/naesi.jpeg', 'rb'), name = 'naesi.jpg', stream_type = 'photo')
+            # if now.hour == 2 or now.hour == 14:
+            #     for data in self._send_group_list:
+            #         self.send_stream_request(self.build_identifier(data), open(os.getcwd() + '/resources/doosi.jpeg', 'rb'), name = 'doosi.jpg', stream_type = 'photo')
 
         self.log.info('타이머는 돌고있다!')
 
-    def activate(self):
-        ### feature - Prepare Politics Talk
-        with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
-            self._data_list = f.read().splitlines()
-            f.close()
-        #####
+    def message_filter(self, send_id, mess, func_name:str, stream_type:str = 'photo'):
+        """
+        특정 키워드를 판별하여 적절한 짤을 보내는 함수
+        정의는 filter.json 에 되어있음
+        """
+        for data in self._data[func_name]:
+            for word in data["filter"]:
+                if mess.body.find(word) != -1:
+                    file_count = len(data["file_name"])
+                    file_name = data["file_name"][random.randrange(0, file_count)]                    
+                    self.send_stream_request(send_id, open(os.getcwd() + '/resources/' + file_name, 'rb'), name = file_name, stream_type = stream_type)
+                    return True
 
-        ### feature - Prepare nesi timer
-        with open('./plugins/err-messagehook/nesi_list.txt', 'r') as f:
-            self._send_group_list = f.read().splitlines()
-            f.close()
-        #####
+        return False
+
+    def activate(self):
+        ### feature - filter load
+        with open('./plugins/err-messagehook/filter.json') as data_file:
+            self._data = json.load(data_file)
+            data_file.close()
+
+        # ### feature - Prepare Politics Talk
+        # with open('./plugins/err-messagehook/win_whitelist.txt', 'r') as f:
+        #     self._data_list = f.read().splitlines()
+        #     f.close()
+        # #####
+
+        # ### feature - Prepare nesi timer
+        # with open('./plugins/err-messagehook/nesi_list.txt', 'r') as f:
+        #     self._send_group_list = f.read().splitlines()
+        #     f.close()
+        # #####
 
         self.start_poller(60, self.nesi_timer)
 
@@ -66,9 +92,11 @@ class MessageHook(BotPlugin):
             send_id = mess.frm
 
         # feature - ~해줘 (feat. 블랙워그레이몬)
-        if mess.body.find('해줘') != -1 or mess.body.find('해줭') != -1 or mess.body.find('부탁') != -1:
-            self.send_stream_request(send_id, open(os.getcwd() + '/resources/deny_new.jpg', 'rb'), name = 'deny_new.jpg', stream_type = 'photo')
+        if self.message_filter(send_id, mess, 'onegai') == True:
             return
+        # if mess.body.find('해줘') != -1 or mess.body.find('해줭') != -1 or mess.body.find('부탁') != -1:
+        #     self.send_stream_request(send_id, open(os.getcwd() + '/resources/deny_new.jpg', 'rb'), name = 'deny_new.jpg', stream_type = 'photo')
+        #     return
 
         # feature - 퇴근 (어딜가)
         if mess.body.find('퇴근') != -1 \
