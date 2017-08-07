9782a5af2f65c3e5299e533184582574356f89fc
oNsemy <onsemy@gmail.com>
2017-08-07 23:25:50 +0900

    [수정] 깃허브로부터 훅받으면 풀과 재시작을 시도...

--- plugins/err-webhook/webhook.py
+++ plugins/err-webhook/webhook.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import json
+import os
 from errbot import BotPlugin, webhook
 
 class PluginExample(BotPlugin):
@@ -7,16 +8,20 @@ class PluginExample(BotPlugin):
         with open('./plugins/err-webhook/settings.json', 'r') as d:
             jsonData = json.load(d)
             self._room_id = jsonData['room_id']
+            self._user_id = jsonData['user_id']
 
         super().activate()
 
     @webhook('/github/', form_param = 'payload')
     def notification(self, payload):
-        for room in self.bot_config.CHATROOM_PRESENCE:
-            self.send(
-                self.build_identifier(room),
-                'Commit on %s!' % payload['repository']['name'],
-            )
+        self.send(self.build_identifier(self._user_id), 'Commit on %s!' % payload['repository']['name'],)
+        os.system('git pull')
+        self.send(self.bot_identifier(), '/restart')
+        # for room in self.bot_config.CHATROOM_PRESENCE:
+        #     self.send(
+        #         self.build_identifier(room),
+        #         'Commit on %s!' % payload['repository']['name'],
+        #     )
 
     @webhook
     def say(self, request):
