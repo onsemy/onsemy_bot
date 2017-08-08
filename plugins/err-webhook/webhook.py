c86f567415555a57b1d8dbbc1c2bac381fd651a1
oNsemy <onsemy@gmail.com>
2017-08-08 22:39:44 +0900

    [수정] 로그

--- plugins/err-webhook/webhook.py
+++ plugins/err-webhook/webhook.py
@@ -9,10 +9,11 @@ class WebHook(BotPlugin):
 
     def refresher(self):
         if self._is_pushed == True:
+            _is_pushed = False
             sleep(3)
+            self.log.info('user_id: ' + self._user_id)
             self.send(self.build_identifier(self._user_id), 'Restarting bot!')
             self.send(self.bot_identifier(), '/restart',)
-            _is_pushed = False
 
     def activate(self):
         with open('./plugins/err-webhook/settings.json', 'r') as d:
