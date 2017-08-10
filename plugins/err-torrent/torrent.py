c6a8f725097b9c69d5b95a95788d72f2e1a85f24
oNsemy <onsemy@gmail.com>
2017-08-11 01:58:39 +0900

    [수정]

--- plugins/err-torrent/torrent.py
+++ plugins/err-torrent/torrent.py
@@ -43,7 +43,7 @@ class Torrent(BotPlugin):
         # headers = {'Content-Type': 'multipart/form-data; boundary=---------------------------6688794727912'}
         yield "Request Torrent Job!"
         result = None
-        result = requests.post(bot_define.TORRENT_URL + 'command/download', params, headers = headers)
+        result = requests.post(bot_define.TORRENT_URL + 'command/download', data=params, headers = headers)
 
         if not result:
             yield "Something has wrong!"
