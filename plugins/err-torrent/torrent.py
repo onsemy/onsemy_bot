434e8f2acbd894773c9de8f3b566aa17abfb93d2
oNsemy <onsemy@gmail.com>
2017-08-11 01:53:18 +0900

    [수정] 헤더 교체

--- plugins/err-torrent/torrent.py
+++ plugins/err-torrent/torrent.py
@@ -39,7 +39,8 @@ class Torrent(BotPlugin):
             return
 
         params = {'urls':args}
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        # headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        headers = {'Content-Type': 'multipart/form-data; boundary=---------------------------6688794727912'}
         yield "Request Torrent Job!"
 
         result = requests.post(bot_define.TORRENT_URL + 'command/download', params, headers = headers)
